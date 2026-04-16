c53b2d7c6b5908a86d768a0adc04532f0d725ac5
RijurikNortheastern <saha.rij@northeastern.edu>
2026-04-15 22:36:20 -0400

    Add sample data: 5 departments

--- DML/01_insert_departments.sql
+++ DML/01_insert_departments.sql
@@ -1,1 +1,10 @@
--- DML: Departments 
+-- Insert Departments
+
+INSERT INTO DEPARTMENT VALUES (department_seq.NEXTVAL, 'Cardiology', 'Building A, Floor 2', '555-0001');
+INSERT INTO DEPARTMENT VALUES (department_seq.NEXTVAL, 'Neurology', 'Building A, Floor 3', '555-0002');
+INSERT INTO DEPARTMENT VALUES (department_seq.NEXTVAL, 'Orthopedics', 'Building B, Floor 1', '555-0003');
+INSERT INTO DEPARTMENT VALUES (department_seq.NEXTVAL, 'Pediatrics', 'Building B, Floor 2', '555-0004');
+INSERT INTO DEPARTMENT VALUES (department_seq.NEXTVAL, 'General Medicine', 'Building C, Floor 1', '555-0005');
+COMMIT;
+
+select * from department;
